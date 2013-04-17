cd10f60d416519f1335d28ce65a0ea128a6b2c6c
Andrew <libraryaddict115@yahoo.co.nz>
2013-04-18 09:58:50 +1200

    Included info on LibsCommands

--- src/me/libraryaddict/Hungergames/Listeners/LibsCommandsListener.java
+++ src/me/libraryaddict/Hungergames/Listeners/LibsCommandsListener.java
@@ -8,6 +8,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class LibsCommandsListener extends Extender implements Listener {
+    
+    /**
+     * This is simply code for my own use. Removes the ability for spectators to private message alive players.
+     * 
+     * Such as revealing someone else is about to attack in a sneak ambush. And ruining the game for the ambusher
+     */
     @EventHandler
     public void onMessage(PrivateMessageEvent event) {
         Gamer gamer = pm.getGamer(event.getSender().getName());
