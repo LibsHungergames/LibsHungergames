7857b904f39c8564bf120ab69862be8acfae41ea
Andrew <libraryaddict115@yahoo.co.nz>
2013-04-20 05:14:42 +1200

    Removed timelord from the list of kits which will be added

---
+++ src/me/libraryaddict/Hungergames/Kits/Flash.java
@@ -0,0 +1,9 @@
+package me.libraryaddict.Hungergames.Kits;
+
+import java.util.HashMap;
+
+public class Flash {
+    
+    private HashMap<String, Integer> cooldown = new HashMap<String, Integer>();
+
+}
