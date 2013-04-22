82452c805419bea6d4a48341f4dd2d414689eef3
Andrew <libraryaddict115@yahoo.co.nz>
2013-04-22 14:12:50 +1200

    Fixed salvager not working and now works perfectly.

--- src/me/libraryaddict/Hungergames/Kits/Salvager.java
+++ src/me/libraryaddict/Hungergames/Kits/Salvager.java
@@ -2,6 +2,7 @@ package me.libraryaddict.Hungergames.Kits;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -13,36 +14,45 @@ import org.bukkit.inventory.ShapelessRecipe;
 
 import me.libraryaddict.Hungergames.Managers.KitManager;
 import me.libraryaddict.Hungergames.Managers.PlayerManager;
+import me.libraryaddict.Hungergames.Types.Enchants;
 import me.libraryaddict.Hungergames.Types.HungergamesApi;
 
-public class Salavager implements Listener {
+public class Salvager implements Listener {
 
     private PlayerManager pm = HungergamesApi.getPlayerManager();
     private KitManager kits = HungergamesApi.getKitManager();
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
+        Player p = event.getPlayer();
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.ANVIL
-                && pm.getGamer(event.getPlayer()).isAlive() && kits.hasAbility(event.getPlayer(), "Salvager")) {
+                && pm.getGamer(p).isAlive() && kits.hasAbility(p, "Salvager")) {
             ItemStack item = event.getItem();
-            if (item != null && item.getType() != Material.AIR) {
+            if (item != null && item.getType() != Material.AIR && item.getType() != Material.ANVIL
+                    && item.getType() != Material.COMPASS && !item.containsEnchantment(Enchants.UNLOOTABLE)) {
                 for (Recipe recipe : Bukkit.getRecipesFor(item)) {
+                    if (recipe.getResult().getAmount() > item.getAmount())
+                        continue;
                     if (recipe instanceof ShapelessRecipe) {
+                        item.setAmount(item.getAmount() - recipe.getResult().getAmount());
+                        if (item.getAmount() <= 0)
+                            p.setItemInHand(new ItemStack(0));
                         event.setCancelled(true);
-                        item.setAmount(0);
-                        item.setDurability((short) 0);
-                        item.setType(Material.AIR);
                         for (ItemStack items : ((ShapelessRecipe) recipe).getIngredientList())
-                            kits.addItem(event.getPlayer(), items.clone());
+                            if (items != null && items.getType() != Material.AIR)
+                                kits.addItem(p, items.clone());
+                        p.updateInventory();
                         break;
                     }
                     if (recipe instanceof ShapedRecipe) {
+                        item.setAmount(item.getAmount() - recipe.getResult().getAmount());
+                        if (item.getAmount() <= 0)
+                            p.setItemInHand(new ItemStack(0));
                         event.setCancelled(true);
-                        item.setAmount(0);
-                        item.setDurability((short) 0);
-                        item.setType(Material.AIR);
                         for (ItemStack items : ((ShapedRecipe) recipe).getIngredientMap().values())
-                            kits.addItem(event.getPlayer(), items.clone());
+                            if (items != null && items.getType() != Material.AIR)
+                                kits.addItem(p, items.clone());
+                        p.updateInventory();
                         break;
                     }
                 }
