d4bc9175874df19afde580d2d22b35239adf59e7
Andrew <libraryaddict115@yahoo.co.nz>
2013-05-08 00:43:25 +1200

    Released 2.1

--- src/me/libraryaddict/Hungergames/Types/AbilityListener.java
+++ src/me/libraryaddict/Hungergames/Types/AbilityListener.java
@@ -4,7 +4,6 @@ import me.libraryaddict.Hungergames.Managers.TranslationManager;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
