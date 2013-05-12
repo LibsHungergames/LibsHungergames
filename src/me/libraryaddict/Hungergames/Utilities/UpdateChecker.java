cc61a1230f6c1aa6c5c7cc3e9e4bd93971a53c71
Andrew <libraryaddict115@yahoo.co.nz>
2013-05-12 21:31:50 +1200

    Changed UpdateChecker to use spigot

--- src/me/libraryaddict/Hungergames/Utilities/UpdateChecker.java
+++ src/me/libraryaddict/Hungergames/Utilities/UpdateChecker.java
@@ -1,11 +1,11 @@
 package me.libraryaddict.Hungergames.Utilities;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.regex.Pattern;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.CharacterData;
-import org.w3c.dom.Element;
 
 public class UpdateChecker {
     private String latestVersion;
@@ -14,12 +14,24 @@ public class UpdateChecker {
         return latestVersion;
     }
 
+    private String getVersion(String key, int resourceId) {
+        String version = null;
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL("http://www.spigotmc.org/api/general.php").openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.getOutputStream().write(("key=" + key + "&resource=" + resourceId).getBytes("UTF-8"));
+            version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return version;
+    }
+
     public void checkUpdate(String currentVersion) throws Exception {
-        String version = ((CharacterData) ((Element) DocumentBuilderFactory.newInstance().newDocumentBuilder()
-                .parse(new URL("http://dev.bukkit.org/server-mods/hunger-games/files.rss").openStream())
-                .getElementsByTagName("item").item(0)).getElementsByTagName("title").item(0).getFirstChild()).getData();
-        if (checkHigher(currentVersion, version))
-            latestVersion = version.replace("Lib's Hungergames ", "");
+        String version = getVersion("98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4", 55);
+        if (checkHigher(currentVersion.replace("v", ""), version))
+            latestVersion = version;
     }
 
     private boolean checkHigher(String currentVersion, String newVersion) {
