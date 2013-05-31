7108615f77369df0043df447e784127897b0204c
Andrew <libraryaddict115@yahoo.co.nz>
2013-06-01 11:22:31 +1200

    Added kit Butcher

---
+++ src/me/libraryaddict/Hungergames/Abilities/Butcher.java
@@ -0,0 +1,26 @@
+package me.libraryaddict.Hungergames.Abilities;
+
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import me.libraryaddict.Hungergames.Types.AbilityListener;
+
+public class Butcher extends AbilityListener {
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player && hasAbility((Player) event.getDamager())) {
+            if (event.getEntity() instanceof Creature) {
+                Creature entity = (Creature) event.getEntity();
+                if (entity.getHealth() > 1) {
+                    event.setDamage(0);
+                    entity.setHealth(1);
+                }
+
+            }
+        }
+    }
+
+}
