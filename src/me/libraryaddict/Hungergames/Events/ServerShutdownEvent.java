62df95cdcdeea599058e9292ed3fbd453161e6e8
Andrew <libraryaddict115@yahoo.co.nz>
2013-06-30 13:14:00 +1200

    ServerShutdownEvent now implements Cancellable

--- src/me/libraryaddict/Hungergames/Events/ServerShutdownEvent.java
+++ src/me/libraryaddict/Hungergames/Events/ServerShutdownEvent.java
@@ -1,26 +1,28 @@
 package me.libraryaddict.Hungergames.Events;
 
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class ServerShutdownEvent extends Event {
+public class ServerShutdownEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
+
     public static HandlerList getHandlerList() {
         return handlers;
     }
-    
+
     private boolean cancelled = false;
-    
+
     public HandlerList getHandlers() {
         return handlers;
     }
-    
+
     public boolean isCancelled() {
         return cancelled;
     }
-    
+
     public void setCancelled(boolean state) {
         cancelled = state;
     }
-    
+
 }
