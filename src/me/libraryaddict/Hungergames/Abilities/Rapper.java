9519f013cd7e51b763c55b9e37c7daeadaaf3d0b
Andrew <libraryaddict115@yahoo.co.nz>
2013-07-31 18:08:46 +1200

    Added kit Rapper

---
+++ src/me/libraryaddict/Hungergames/Abilities/Rapper.java
@@ -0,0 +1,36 @@
+package me.libraryaddict.Hungergames.Abilities;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import me.libraryaddict.Hungergames.Interfaces.Disableable;
+import me.libraryaddict.Hungergames.Types.AbilityListener;
+
+public class Rapper extends AbilityListener implements Disableable {
+    public int nauseaLength = 5;
+    public int hearingRange = 16;
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (hasAbility(event.getPlayer())) {
+            if (event.getAction().name().contains("RIGHT")) {
+                if (event.getClickedBlock().getType() == Material.NOTE_BLOCK) {
+                    for (Entity entity : event.getPlayer().getNearbyEntities(hearingRange, hearingRange, hearingRange)) {
+                        if (entity instanceof LivingEntity) {
+                            ((LivingEntity) entity).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION,
+                                    nauseaLength * 20, 0), true);
+                        }
+                    }
+                    ((LivingEntity) event.getPlayer()).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION,
+                            nauseaLength * 20, 0), true);
+                }
+            }
+        }
+    }
+
+}
