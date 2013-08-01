30522c7ec802636056c6536a57f073f8b4682a98
Andrew <libraryaddict115@yahoo.co.nz>
2013-08-01 14:05:35 +1200

    Fix for giving nausea to live gamers

--- src/me/libraryaddict/Hungergames/Abilities/Rapper.java
+++ src/me/libraryaddict/Hungergames/Abilities/Rapper.java
@@ -3,6 +3,7 @@ package me.libraryaddict.Hungergames.Abilities;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.potion.PotionEffect;
@@ -10,6 +11,7 @@ import org.bukkit.potion.PotionEffectType;
 
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
+import me.libraryaddict.Hungergames.Types.HungergamesApi;
 
 public class Rapper extends AbilityListener implements Disableable {
     public int nauseaLength = 5;
@@ -21,7 +23,7 @@ public class Rapper extends AbilityListener implements Disableable {
             if (event.getAction().name().contains("RIGHT")) {
                 if (event.getClickedBlock().getType() == Material.NOTE_BLOCK) {
                     for (Entity entity : event.getPlayer().getNearbyEntities(hearingRange, hearingRange, hearingRange)) {
-                        if (entity instanceof LivingEntity) {
+                        if (!(entity instanceof Player) || HungergamesApi.getPlayerManager().getGamer(entity).isAlive()) {
                             ((LivingEntity) entity).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION,
                                     nauseaLength * 20, 0), true);
                         }
