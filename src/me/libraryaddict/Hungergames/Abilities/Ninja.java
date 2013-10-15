e745c7d9501d5a73c5df6bca9045b9228f4a293a
Andrew <libraryaddict115@yahoo.co.nz>
2013-10-15 21:51:18 +1300

    Ninja now follows world time

--- src/me/libraryaddict/Hungergames/Abilities/Ninja.java
+++ src/me/libraryaddict/Hungergames/Abilities/Ninja.java
@@ -4,6 +4,7 @@ import me.libraryaddict.Hungergames.Events.GameStartEvent;
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.potion.PotionEffect;
@@ -12,32 +13,34 @@ import org.bukkit.potion.PotionEffectType;
 import me.libraryaddict.Hungergames.Types.HungergamesApi;
 
 public class Ninja extends AbilityListener implements Disableable {
-    public String[] potionEffectsDay = new String[] { "SPEED 12000 0" };
-    public String[] potionEffectsNight = new String[] { "NIGHT_VISION 12000 0" };
+    public String[] potionEffectsDuringDay = new String[] { "SPEED 0" };
+    public String[] potionEffectsDuringNight = new String[] { "NIGHT_VISION 0" };
     private int scheduler = -1;
 
     @EventHandler
     public void gameStartEvent(GameStartEvent event) {
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(HungergamesApi.getHungergames(), getRunnable(), 0, 12000);
+        getRunnable().run();
+        registerScheduler();
     }
 
     private Runnable getRunnable() {
         return new Runnable() {
             public void run() {
+                World world = HungergamesApi.getHungergames().world;
+                int timeTillNextSwitch = (int) (12000 - (world.getTime() % 12000));
                 for (Player p : getMyPlayers()) {
-                    if (HungergamesApi.getHungergames().world.getTime() >= 0
-                            && HungergamesApi.getHungergames().world.getTime() <= 12000) {
-                        for (String string : potionEffectsDay) {
+                    if (world.getTime() >= 0 && world.getTime() < 12000) {
+                        for (String string : potionEffectsDuringDay) {
                             String[] effect = string.split(" ");
                             PotionEffect potionEffect = new PotionEffect(PotionEffectType.getByName(effect[0].toUpperCase()),
-                                    Integer.parseInt(effect[1]), Integer.parseInt(effect[2]));
+                                    timeTillNextSwitch, Integer.parseInt(effect[1]));
                             p.addPotionEffect(potionEffect, true);
                         }
                     } else {
-                        for (String string : potionEffectsNight) {
+                        for (String string : potionEffectsDuringNight) {
                             String[] effect = string.split(" ");
                             PotionEffect potionEffect = new PotionEffect(PotionEffectType.getByName(effect[0].toUpperCase()),
-                                    Integer.parseInt(effect[1]), Integer.parseInt(effect[2]));
+                                    timeTillNextSwitch, Integer.parseInt(effect[1]));
                             p.addPotionEffect(potionEffect, true);
                         }
                     }
@@ -46,10 +49,23 @@ public class Ninja extends AbilityListener implements Disableable {
         };
     }
 
+    private void registerScheduler() {
+        int timeTillNextSwitch = (int) (12000 - (HungergamesApi.getHungergames().world.getTime() % 12000));
+        scheduler = Bukkit.getScheduler().scheduleSyncRepeatingTask(HungergamesApi.getHungergames(), getRunnable(),
+                timeTillNextSwitch, 12000);
+    }
+
     public void registerPlayer(Player player) {
         super.registerPlayer(player);
         if (scheduler < 0 && HungergamesApi.getHungergames().currentTime >= 0)
-            scheduler = Bukkit.getScheduler().scheduleSyncRepeatingTask(HungergamesApi.getHungergames(), getRunnable(), 0,
-                    12000 - (HungergamesApi.getHungergames().world.getTime() % 12000));
+            registerScheduler();
+    }
+
+    public void unregisterPlayer(Player player) {
+        super.unregisterPlayer(player);
+        if (scheduler >= 0 && getMyPlayers().isEmpty()) {
+            Bukkit.getScheduler().cancelTask(scheduler);
+            scheduler = -1;
+        }
     }
 }
