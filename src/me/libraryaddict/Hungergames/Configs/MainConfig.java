99f10ac519d6ed2c4ed72456cc258cbb1c8a260d
Andrew <libraryaddict115@yahoo.co.nz>
2013-10-21 03:58:18 +1300

    Fixed not being able to set the time the plugin sets the world to on load

--- src/me/libraryaddict/Hungergames/Configs/MainConfig.java
+++ src/me/libraryaddict/Hungergames/Configs/MainConfig.java
@@ -300,6 +300,7 @@ public class MainConfig extends BaseConfig {
             System.out.println(String.format(cm.getChangedWorldHeight(), this.getMaxHeightLimit()));
         }
         Hungergames hg = HungergamesApi.getHungergames();
+        hg.currentTime = this.getTimeGameIsSetToWhenEnabled();
         currentVersion = "v" + hg.getDescription().getVersion();
         if (checkUpdates)
             Bukkit.getScheduler().scheduleAsyncDelayedTask(hg, new Runnable() {
