9414dbda1a5ff192b79a269f18c82b44768c3371
ArkhamNetwork Dev <devane1249@Gmail.com>
2013-12-23 12:55:13 +0000

    Use @Override (Fix warnings in some IDE's)

--- src/me/libraryaddict/Hungergames/Hungergames.java
+++ src/me/libraryaddict/Hungergames/Hungergames.java
@@ -176,6 +176,7 @@ public class Hungergames extends JavaPlugin {
         return true;
     }
 
+    @Override
     public void onDisable() {
         for (Player p : Bukkit.getOnlinePlayers()) {
             p.kickPlayer(translationsConfig.getKickGameShutdownUnexpected());
@@ -186,6 +187,7 @@ public class Hungergames extends JavaPlugin {
         HungergamesApi.getMySqlManager().getPlayerJoinThread().stop();
     }
 
+    @Override
     public void onEnable() {
         HungergamesApi.init(this);
         ConfigManager config = HungergamesApi.getConfigManager();
