23b6b01c9f2f078972828259f7966cd620883ff3
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-05-10 09:38:12 +1200

    Allow them to say when it was purchased

--- src/me/libraryaddict/Hungergames/Types/GiveKitThread.java
+++ src/me/libraryaddict/Hungergames/Types/GiveKitThread.java
@@ -11,6 +11,7 @@ import me.libraryaddict.Hungergames.Configs.MySqlConfig;
 
 public class GiveKitThread extends Thread {
     private Connection con = null;
+    private Timestamp timestamp;
     private String kitName;
     private String playerName;
     private String uuid;
@@ -19,6 +20,14 @@ public class GiveKitThread extends Thread {
         this.uuid = uuid;
         kitName = kit;
         playerName = player;
+        timestamp = new Timestamp(new Date().getTime());
+    }
+
+    public GiveKitThread(String player, String uuid, String kit, Timestamp timestamp) {
+        this.uuid = uuid;
+        this.kitName = kit;
+        this.playerName = player;
+        this.timestamp = timestamp;
     }
 
     public void mySqlConnect() {
@@ -45,11 +54,12 @@ public class GiveKitThread extends Thread {
             return;
         mySqlConnect();
         try {
-            PreparedStatement stmt = con.prepareStatement("INSERT INTO HGKits (`uuid`, `Name`, `KitName`, `Date`) VALUES (?,?,?,?)");
+            PreparedStatement stmt = con
+                    .prepareStatement("INSERT INTO HGKits (`uuid`, `Name`, `KitName`, `Date`) VALUES (?,?,?,?)");
             stmt.setString(1, uuid);
             stmt.setString(2, playerName);
             stmt.setString(3, kitName);
-            stmt.setTimestamp(4, new Timestamp(new Date().getTime()));
+            stmt.setTimestamp(4, timestamp);
             stmt.execute();
             stmt.close();
         } catch (Exception ex) {
