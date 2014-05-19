a251a7453f5afdf35a1b9ae194b9b614963426ed
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-05-20 06:15:05 +1200

    Fix it to do first time as well

--- src/me/libraryaddict/Hungergames/Abilities/ItemRegen.java
+++ src/me/libraryaddict/Hungergames/Abilities/ItemRegen.java
@@ -11,21 +11,26 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
 
 public class ItemRegen extends AbilityListener implements Disableable {
-    public String[] itemsToRegen = new String[] { "SecondsBetweenItems KitName ItemType ItemDurability ItemAmount ItemData",
-            "300 libraryaddict BOOK 0 1 Unlootable 1 Name=Explosive_Reading" };
+    public String[] itemsToRegen = new String[] {
+            "FirstItemGivenAt SecondsBetweenItems KitName ItemType ItemDurability ItemAmount ItemData",
+            "300 300 libraryaddict BOOK 0 1 Unlootable 1 Name=Explosive_Reading" };
 
     @EventHandler
     public void onExplode(TimeSecondEvent event) {
+        int currentTime = HungergamesApi.getHungergames().currentTime;
         for (String string : itemsToRegen) {
             String[] split = string.split(" ");
             try {
-                if (Integer.parseInt(split[0]) != HungergamesApi.getHungergames().currentTime)
+                int firstTime = Integer.parseInt(split[0]);
+                if (firstTime < currentTime ? ((currentTime - firstTime) % Integer.parseInt(split[1])) != 0
+                        : firstTime != currentTime) {
                     continue;
+                }
             } catch (Exception ex) {
                 continue;
             }
             ItemStack[] items = HungergamesApi.getKitManager().parseItem(
-                    string.substring(2 + split[1].length() + split[0].length()));
+                    string.substring(3 + split[0].length() + split[1].length() + split[2].length()));
             for (Player player : this.getMyPlayers()) {
                 Kit kit = HungergamesApi.getKitManager().getKitByPlayer(player);
                 if (kit != null) {
